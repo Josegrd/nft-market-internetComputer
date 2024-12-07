b260828c94dfb0341ec484a5b954653a457b31d1
Jose Garda Seta <jose8gard@gmail.com>
2024-12-07 19:18:29 +0700

    [ADD] frontend react, backend motoko for blockchain

---
+++ src/NFT/nft.mo
@@ -0,0 +1,26 @@
+import Debug "mo:base/Debug";
+import Principal "mo:base/Principal";
+import Text "mo:base/Text";
+
+actor class NFT (name: Text, owner: Principal, image: [Nat8]) = this {
+    
+    let itemName = name;
+    let nftOwner = owner;
+    let imageBytes = image;
+
+    public query func getName(): async Text {
+        return itemName;
+    };
+
+    public query func getOwner(): async Principal {
+        return nftOwner;
+    };
+
+    public query func getImage(): async [Nat8] {
+        return imageBytes;
+    };
+
+    public query func getCanisterId(): async Principal {
+        return Principal.fromActor(this);
+    };
+};
---
+++ src/opend_assets/src/components/Item.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/logo.png";
+import {Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { Principal } from "@dfinity/principal";
+
+function Item(props) {
+  const id = Principal.fromText(props.id);
+  const localHost = "http://localhost:8000";
+  const agent = new HttpAgent({ host: localHost });
+  const [name, setName] = useState("");
+  const [principal, setPrincipal] = useState(null)
+  const [image, setImage] = useState("");
+
+  async function loadNFT() {
+    const NFTActor = await Actor.createActor(idlFactory,{
+      agent,
+      canisterId: id
+    });
+    const name = await NFTActor.getName();
+    setName(name);
+    const principal = await NFTActor.getOwner();
+    setPrincipal(principal.toText());
+    const imageData = await NFTActor.getImage();
+    const imageContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(new Blob([imageContent], { type: "image/png" }));
+    setImage(image);
+  }
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
+  return (
+    <div className="disGrid-item">
+      <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
+        <img
+          className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
+          src={image}
+        />
+        <div className="disCardContent-root">
+          <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
+            {name}<span className="purple-text"></span>
+          </h2>
+          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
+            <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
+              Owner: {principal}
+            </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
